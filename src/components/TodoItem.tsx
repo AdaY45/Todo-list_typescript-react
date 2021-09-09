ba57059e6526379e031584f5d4beee6228f1630d
AdaY45 <afsdert2012@gmail.com>
2021-09-09 23:39:08 +0300

    add check of todo and mobile version

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,17 +1,35 @@
+import { useState } from "react";
 import Todo from "../models/todo";
 import classes from "./TodoItem.module.css";
 
-const Item: React.FC<{ item: Todo, onDeleteTodo: (id: string) => void }> = (props) => {
-    const deleteTodo = (event: React.MouseEvent) => {
-        event.preventDefault();
+const Item: React.FC<{
+  item: Todo;
+  onDeleteTodo: (id: string) => void;
+}> = (props) => {
+  const [isChecked, setIsChecked] = useState<boolean>(false);
 
-        props.onDeleteTodo(props.item.id);
-    }
+  const checkedHandler = () => {
+    setIsChecked(!isChecked);
+  };
+
+  const deleteTodo = (event: React.MouseEvent) => {
+    event.preventDefault();
+
+    props.onDeleteTodo(props.item.id);
+  };
 
   return (
-    <li className={classes["todo-item"]} key={props.item.id}>
+    <li
+      onClick={checkedHandler}
+      className={ `${classes["todo-item"]} ${isChecked ? classes['checked'] : ''}`
+        // isChecked ? classes["todo-item"] + " " + classes.checked : classes["todo-item"]
+      }
+      key={props.item.id}
+    >
       <div className={classes["todo-text"]}>{props.item.text}</div>
-      <button className={classes.cancel} onClick={deleteTodo}>x</button>
+      <button className={classes.cancel} onClick={deleteTodo}>
+        x
+      </button>
     </li>
   );
 };
